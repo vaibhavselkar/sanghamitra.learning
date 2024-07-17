3f9c5480020e9f28d6ff0c3238853819682b05af
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2024-07-17 16:05:33 +0530

    Update userSchema.js

--- Server/model/userSchema.js
+++ Server/model/userSchema.js
@@ -26,15 +26,7 @@ const userSchema = new mongoose.Schema({
                 required: true,
             }
         }
-    ],
-    loginHistory: [
-    {
-      loginTimestamp: {
-        type: Date,
-        default: Date.now
-      }
-    }
-  ]
+    ]
 });
 
 // we are hashing the password
