485dccedd58127705c5b7e70e21576dc17329b60
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2024-07-22 08:23:02 +0530

    Update auth.js

--- Server/router/auth.js
+++ Server/router/auth.js
@@ -64,6 +64,8 @@ router.post('/signin', async (req, res) => {
         const token = await user.generateAuthToken();
         req.session.userId = user._id;
         req.session.token = token;
+        req.session.username = user.username;
+        req.session.email = user.email;
         res.status(200).json({ message: 'User signed in successfully' });
 
     } catch (error) {
