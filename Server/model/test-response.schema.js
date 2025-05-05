38ce6be703e9b2e67de4a6e6dc7a64663db6ea90
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2025-05-05 22:46:44 +0530

    Create test-response.schema.js

---
+++ Server/model/test-response.schema.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const testResponseSchema = new mongoose.Schema({
+    userEmail: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    testDate: {
+        type: Date,
+        default: Date.now
+    },
+    responses: [{
+        questionData: {
+            questionText: String,
+            questionOptions: {
+                optionA: String,
+                optionB: String,
+                optionC: String,
+                optionD: String
+            },
+            questionCorrectAnswer: String,
+            explanationText: String,
+            questionDifficulty: String,
+            questionTopicArea: String,
+            questionTopic: String,
+            testedConcepts: [String],
+            questionMisconceptions: [String],
+            averageTime: Number,
+            prerequisiteTopics: [String],
+            gradeLevel: Number
+        },
+        userAnswer: {
+            type: String,
+            enum: ['a', 'b', 'c', 'd', 'unanswered'],
+            required: true
+        },
+        timeSpent: {
+            type: mongoose.Schema.Types.Mixed,
+            validate: {
+                validator: function(v) {
+                    return typeof v === 'number' || v === 'unseen';
+                },
+                message: props => `${props.value} is not a valid time spent value!`
+            },
+            required: true
+        }
+    }]
+});
+
+const TestResponse = mongoose.model('TestResponse', testResponseSchema);
+module.exports = TestResponse;
