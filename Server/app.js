1dd7e99158e7fa1dda268021de8e0768f3e3d2c1
Vaibhav Selkar <132983753+vaibhavselkar@users.noreply.github.com>
2026-04-02 13:19:54 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -15,7 +15,7 @@ dotenv.config({ path: './.env' });
 
 // Middleware setup
 app.use(cors({
-    origin: ['https://sanghamitra-learnworld.vercel.app/api/','https://iitm.sanghamitra.com',]
+    origin: ['https://sanghamitra-learnworld.vercel.app/api/','https://iitm.sanghamitra.com',],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
